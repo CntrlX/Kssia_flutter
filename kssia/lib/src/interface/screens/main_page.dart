19b22c5af6da6fe6a34095c9c5120462a6afff08
Noora Sajil <123862717+CntrlQ@users.noreply.github.com>
2024-07-23 16:25:12 +0530

    two

--- kssia/lib/src/interface/screens/main_page.dart
+++ kssia/lib/src/interface/screens/main_page.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+void main() {
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page', style: TextStyle(fontSize: 24)),
+    Text('Feed Page', style: TextStyle(fontSize: 24)),
+    Text('Profile Page', style: TextStyle(fontSize: 24)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,10 +33,7 @@ class MainPage extends StatelessWidget {
         title: Text('Main Page'),
       ),
       body: Center(
-        child: Text(
-          'Welcome to the Main Page!',
-          style: TextStyle(fontSize: 24),
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -20,14 +42,17 @@ class MainPage extends StatelessWidget {
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
+            icon: Icon(Icons.feed),
+            label: 'Feed',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: 'Profile',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
