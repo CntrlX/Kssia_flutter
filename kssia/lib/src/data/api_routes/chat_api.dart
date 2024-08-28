4bddefaaca03009ead6b86693b1c8226a00f6fb0
Nihal anil <43931373+CntrlX@users.noreply.github.com>
2024-08-28 21:12:15 +0530

    extra changes to code

--- kssia/lib/src/data/api_routes/chat_api.dart
+++ kssia/lib/src/data/api_routes/chat_api.dart
@@ -1,68 +1,157 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:developer';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:web_socket_client/web_socket_client.dart';
+import 'package:http/http.dart' as http;
+import 'package:kssia/src/data/globals.dart';
+import 'package:kssia/src/data/models/chat_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:kssia/src/data/models/msg_model.dart';
 
-// Define a WebSocket client provider
-final webSocketClientProvider = Provider<WebSocketClient>((ref) {
-  return WebSocketClient();
+part 'chat_api.g.dart';
+
+// Define a Socket.IO client providerr
+final socketIoClientProvider = Provider<SocketIoClient>((ref) {
+  return SocketIoClient();
 });
 
 // Define a message stream provider
 final messageStreamProvider = StreamProvider.autoDispose<MessageModel>((ref) {
-  final webSocketClient = ref.read(webSocketClientProvider);
-  return webSocketClient.messageStream;
+  final socketIoClient = ref.read(socketIoClientProvider);
+  return socketIoClient.messageStream;
 });
 
-class WebSocketClient {
-  late WebSocket _webSocket;
+class SocketIoClient {
+  late IO.Socket _socket;
   final _controller = StreamController<MessageModel>();
 
-  WebSocketClient();
+  SocketIoClient();
 
   Stream<MessageModel> get messageStream => _controller.stream;
 
-  Future<void> connect(String receiverId) async {
-    final uri = Uri.parse('ws://43.205.89.79/api/v1/chats/send/$receiverId');
-    _webSocket = WebSocket(uri);
+  void connect(String receiverId, String senderId) {
+    final uri = 'http://43.205.89.79/api/v1/chats?userId=$senderId'; // Base URI
 
-    // Listen to messages from the server
-    _webSocket.messages.listen(
-      (message) {
-        final decodedMessage = jsonDecode(message);
-        print('Received message: $decodedMessage');
-        final messageModel = MessageModel.fromJson(decodedMessage);
-        _controller.add(messageModel);
-      },
-      onDone: () {
-        _controller.close();
-      },
-      onError: (error) {
-        _controller.addError(error);
-      },
+    // Initialize socket.io client
+    _socket = IO.io(
+      uri,
+      IO.OptionBuilder()
+          .setTransports(['websocket']) // Use WebSocket transport
+          .setExtraHeaders({'receiver_id': receiverId}) // Optional headers
+          .disableAutoConnect() // Disable auto-connect
+          .build(),
     );
 
-    // Wait until the connection is established
-    await _webSocket.connection.firstWhere((state) => state is Connected);
-  }
+    log('Connecting to: $uri');
 
-  void sendMessage(String senderId, String message) {
-    final msg = jsonEncode({
-      'sender_id': senderId,
-      'message': message,
+    // Listen for connection events
+    _socket.onConnect((_) {
+      log('Connected to: $uri');
     });
-    _webSocket.send(msg);
+
+    // Listen to messages from the server
+    _socket.on('message', (data) {
+      final decodedMessage = jsonDecode(data);
+      print('Received message: $decodedMessage');
+      final messageModel = MessageModel.fromJson(decodedMessage);
+      _controller.add(messageModel);
+    });
+
+    // Handle connection errors
+    _socket.on('connect_error', (error) {
+      print('Connection Error: $error');
+      _controller.addError(error);
+    });
+
+    // Handle disconnection
+    _socket.onDisconnect((_) {
+      print('Disconnected from server');
+      _controller.close();
+    });
+
+    // Connect manually
+    _socket.connect();
   }
 
-  Future<void> disconnect() async {
-    _webSocket.close(1000, 'CLOSE_NORMAL');
+  void disconnect() {
+    _socket.disconnect();
+    _socket.dispose(); // To prevent memory leaks
   }
 }
 
-Future<void> fetchChatThread(String userId, String token) async {
-  final url = Uri.parse('http://43.205.89.79/api/v1/chats/threads/$userId');
+Future<void> sendChatMessage({
+  required String userId,
+  required String from,
+  required String content,
+  String? attachments,
+}) async {
+  final String url = 'http://43.205.89.79/api/v1/chats/send/$userId';
+
+  var request = http.MultipartRequest('POST', Uri.parse(url))
+    ..fields['content'] = content;
+
+  if (attachments != null && attachments.isNotEmpty) {
+    request.files.add(await http.MultipartFile.fromPath(
+      'attachments',
+      attachments,
+    ));
+  }
+  request.headers.addAll({
+    'accept': 'application/json',
+    'Content-Type': 'multipart/form-data',
+    'Authorization': 'Bearer ${token}',
+  });
+
+  log('Sending message to: $url');
+
+  log('Request headers: ${request.headers}');
+  try {
+    final response = await request.send();
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      print('Message sent successfully');
+    } else {
+      print('Failed to send message: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error sending message: $e');
+  }
+}
+
+Future<List<MessageModel>> getMessages(
+    String senderId, String recieverId) async {
+  final String url =
+      'http://43.205.89.79/api/v1/chats/messages/${senderId}/${recieverId}';
+  final response = await http.get(
+    Uri.parse(url),
+    headers: {
+      'accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // Parse the JSON data
+    final messages = json.decode(response.body)['data'];
+    // Handle the messages data as per your needs
+
+    print(messages);
+
+    return messages
+        .map<MessageModel>((item) => MessageModel.fromJson(item))
+        .toList();
+  } else {
+    // Handle the error
+    print('Failed to load messages. Status code: ${response.body}');
+    throw Exception('Failed to load messages');
+  }
+}
+
+@riverpod
+Future<List<ChatModel>> fetchChatThread(
+    FetchChatThreadRef ref, String token) async {
+  final url = Uri.parse('http://43.205.89.79/api/v1/chats/threads');
   print('Requesting URL: $url');
 
   final response = await http.get(
@@ -73,9 +162,14 @@ Future<void> fetchChatThread(String userId, String token) async {
     },
   );
 
-  if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    print('Response data: $data');
+  if (response.statusCode == 200 || response.statusCode == 201) {
+    final data = json.decode(response.body)['data'];
+    log('Response data: $data');
+    final chats =
+        await data.map<ChatModel>((item) => ChatModel.fromJson(item)).toList();
+    
+
+    return chats;
   } else {
     print('Error: ${json.decode(response.body)['message']}');
     throw Exception(json.decode(response.body)['message']);
--- kssia/lib/src/data/globals.dart
+++ kssia/lib/src/data/globals.dart
@@ -3,4 +3,4 @@ library data;
 String id = '66c38e4db9aa147c230339bf';
 
 String token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcklkIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.gw7m0eu3gxSoavEQa4aIt48YZVQz_EsuZ0nJDrjXKuI';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJJZCI6IjY2YzQzZDA5ZTY2NDcwNTJhODkyMjYzZSJ9LCJpYXQiOjE3MjQ0MDE5NzZ9.oD5xQQmsiqBy9lq7qx9ch74v0anC7szu7W2nlVotTk4';
