1d46ad89aa985f359b981a4439eb2a0e69260f1f
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2025-02-15 12:51:05 +0530

    save contact permission

--- kssia/lib/src/data/services/save_contact.dart
+++ kssia/lib/src/data/services/save_contact.dart
@@ -4,14 +4,13 @@ import 'package:contact_add/contact.dart';
 import 'package:contact_add/contact_add.dart';
 import 'package:flutter/material.dart';
 import 'package:kssia/src/interface/common/components/snackbar.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 Future<void> saveContact(
     {required String number,
    required String name,required String email,
     required BuildContext context}) async {
-  // Request permission to access contacts
-  if (await Permission.contacts.request().isGranted) {
+
+
     final Contact contact = Contact(
         firstname: name,
 
@@ -25,8 +24,5 @@ Future<void> saveContact(
     } else {
       CustomSnackbar.showSnackbar(context, 'Contact saving failed!');
     }
-  } else {
-    // Show error message if permission is denied
-    CustomSnackbar.showSnackbar(context, 'Permission denied to save contacts');
-  }
+ 
 }
